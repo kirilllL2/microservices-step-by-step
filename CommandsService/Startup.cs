0b72b727c9ecb323c3610700eec592dd08c68ba6
Кирилл Коннов <k-konnov@list.ru>
2022-05-16 12:43:47 +0400

    register event processor

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CommandsService.Data;
+using CommandsService.EventProcessing;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,7 +32,8 @@ namespace CommandsService
                 opt.UseInMemoryDatabase("InMemory"));
 
             services.AddScoped<ICommandRepo, CommandRepo>();
-            
+
+            services.AddSingleton<IEventProcessor, EventProcessor>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             
             services.AddControllers();
