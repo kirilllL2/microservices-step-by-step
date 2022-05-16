56bf998b1c9b8708f36e1f44b03f7c2d59437e9e
Кирилл Коннов <k-konnov@list.ru>
2022-05-16 12:04:29 +0400

    Add and implement ExternalPlatformExist method

--- CommandsService/Data/ICommandRepo.cs
+++ CommandsService/Data/ICommandRepo.cs
@@ -11,6 +11,7 @@ namespace CommandsService.Data
         IEnumerable<Platform> GetAllPlatforms();
         void CreatePlatform(Platform platform);
         bool PlatformExists(int platformId);
+        bool ExternalPlatformExist(int externalPlatformId);
 
         IEnumerable<Command> GetCommandsForPlatform(int platformId);
         Command GetCommand(int platformId, int commandId);
