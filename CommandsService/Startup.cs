799a54e4157d2bf120c5993fcd5af965ab66379e
Кирилл Коннов <k-konnov@list.ru>
2022-05-18 14:09:40 +0400

    Add GrpcPlatformClient

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -1,19 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace CommandsService
@@ -33,7 +28,7 @@ namespace CommandsService
                 opt.UseInMemoryDatabase("InMemory"));
 
             services.AddScoped<ICommandRepo, CommandRepo>();
-
+            services.AddScoped<IPlatformDataClient, PlatformDataClient>();
             services.AddHostedService<MessageBusSubscriber>();
                         
             services.AddSingleton<IEventProcessor, EventProcessor>();
