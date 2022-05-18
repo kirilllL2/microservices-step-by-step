3ca49e1ba46add2682edc7324187871a58448305
Кирилл Коннов <k-konnov@list.ru>
2022-05-18 12:37:35 +0400

    Field mapping

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -9,7 +9,14 @@ namespace PlatformService.Profiles
         public PlatformsProfile()
         {
             CreateMap<Platform, PlatformReadDto>();
-            CreateMap<Platform, GrpcPlatformModel>();
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(dest => dest.PlatformId,  opt =>
+                    opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt =>
+                    opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Publisher, opt =>
+                    opt.MapFrom(src => src.Publisher)); 
+            
             CreateMap<PlatformCreateDto, Platform>();
             CreateMap<PlatformReadDto, PlatformPublishedDto>();
         }
